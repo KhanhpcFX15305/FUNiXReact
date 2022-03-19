710891799f3b6b6f73877f4ee2a4376cf8d73c59
Khanh Pham <khanhpcFX15305@funix.edu.vn>
2022-03-19 15:40:06 +0700

    show detail staff

--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardText, CardBody } from 'reactstrap';
+import { Card, CardText, CardImg } from 'reactstrap';
 import dateFormat from 'dateformat';
 import { STAFFS } from '../shared/staffs';
 
@@ -7,29 +7,51 @@ class StaffList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nhanvien : STAFFS
+            staffs : STAFFS,
+            detailStaff : null
         }
     }
-
-
+    detailStaff(nv) {
+        this.setState({
+            detailStaff : nv
+        });
+    }
+    renderDetail(nv) {
+        if (nv) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={nv.image} alt={nv.name} />
+                    <CardText>Họ tên : {nv.name}</CardText>
+                    <CardText>Ngày sinh : {dateFormat( nv.doB, "dd/mm/yyyy" )}</CardText>
+                    <CardText>Ngày vào công ty : {dateFormat( nv.startDate, "dd/mm/yyyy")}</CardText>
+                    <CardText>Phòng ban : {nv.department.name}</CardText>
+                    <CardText>Số ngày nghỉ còn lại : {nv.annualLeave}</CardText> 
+                    <CardText>Số ngày làm thêm : {nv.overTime}</CardText>
+                </Card>
+            )
+        }
+    }
     render() {
-        var list = this.state.nhanvien.map((staff) => {
-            console.log(staff);
+        var list = this.state.staffs.map((staff) => {
             return (
                 <div className="col-lg-4 col-md-6 col-xs-12">
-                    <Card>
+                    <Card key={staff.id} onClick={() => {this.detailStaff(staff)}}>
                         <CardText>{staff.name}</CardText>
                     </Card>     
                 </div>
             )
         });
         return (
+        <div>
             <div className="row">
                 {list}
             </div>
+            <div className="row col-lg-4 col-xs-12">
+                {this.renderDetail(this.state.detailStaff)}
+            </div>
+            
+        </div>
         )
     }
 }
-
-
 export default StaffList

9e9263e851f4b01ad3cb1450d0ea4fba9282a0cf
Khanh Pham <khanhpcFX15305@funix.edu.vn>
2022-03-19 15:02:18 +0700

    show list staff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import React, { Component} from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import StaffList from './components/StaffListComponent'
 
 class App extends Component {
+  constructor(props){
+    super(props);
+  }
   render() {
     return (
-      <div>
+      <div className="container">
         <Navbar dark color="primary">
           <NavbarBrand>
             Ứng dụng quản lý nhân viên V1.0
           </NavbarBrand>
         </Navbar>
+        <StaffList />
       </div>
     );
   }
--- src/components/StaffListComponent.js
+++ src/components/StaffListComponent.js
@@ -4,16 +4,31 @@ import dateFormat from 'dateformat';
 import { STAFFS } from '../shared/staffs';
 
 class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            nhanvien : STAFFS
+        }
+    }
 
-    this.state = {
-        nhanvien : STAFFS
-     }
 
-     render() {
+    render() {
+        var list = this.state.nhanvien.map((staff) => {
+            console.log(staff);
+            return (
+                <div className="col-lg-4 col-md-6 col-xs-12">
+                    <Card>
+                        <CardText>{staff.name}</CardText>
+                    </Card>     
+                </div>
+            )
+        });
         return (
-            <div></div>
-        );
-     }
+            <div className="row">
+                {list}
+            </div>
+        )
+    }
 }
 
 

c9d5e1b88483e0ebb8680eea57260e97b2e0b361
Khanh Pham <khanhpcFX15305@funix.edu.vn>
2022-03-19 14:42:35 +0700

    create navbar and start npm

---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Card, CardText, CardBody } from 'reactstrap';
+import dateFormat from 'dateformat';
+import { STAFFS } from '../shared/staffs';
+
+class StaffList extends Component {
+
+    this.state = {
+        nhanvien : STAFFS
+     }
+
+     render() {
+        return (
+            <div></div>
+        );
+     }
+}
+
+
+export default StaffList
